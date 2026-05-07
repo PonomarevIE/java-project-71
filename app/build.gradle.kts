76f089048dcfd40014f40823c360897e1840656a
Glave <ngla@mail.ru>
2026-05-07 12:30:51 +0300

    init gradle project

---
+++ app/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("com.github.ben-manes.versions") version "0.53.0"
+    checkstyle
+    application
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
